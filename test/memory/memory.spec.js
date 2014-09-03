17851a963a36ae114244c15649de3f4231610046
Yarin Podoler <yarin.podler@startapp.com>
2014-09-03 13:44:35 -0400

    Added Tests

---
+++ test/memory/memory.spec.js
@@ -0,0 +1,46 @@
+'use strict';
+
+describe('Memory Interface Unit-Test', function(){
+    var memory,
+        currentModule,
+        sinon = require('sinon');
+
+    beforeEach(function(){
+        memory = require('../../lib/memory/memory');
+    });
+
+    it ('should verify interface exists', function(){
+        expect(typeof memory.get).toEqual('function');
+        expect(typeof memory.set).toEqual('function');
+        expect(typeof memory.use).toEqual('function');
+    });
+
+    describe('Methods Test', function(){
+
+        beforeEach(function(){
+            // Mock currentModule
+            currentModule = sinon.stub({
+                get : function(){},
+                set : function(){}
+            });
+
+            memory.use(currentModule);
+        });
+
+        it ('should verify get method', function(){
+            var callback = function(){};
+
+            memory.get('key', callback);
+
+            expect(currentModule.get.calledWith('key', callback)).toBeTruthy();
+        });
+
+        it ('should verify set method', function(){
+            var done = function(){};
+
+            memory.set('key', 'value', done);
+
+            expect(currentModule.set.calledWith('key', 'value', done)).toBeTruthy();
+        });
+    });
+});
