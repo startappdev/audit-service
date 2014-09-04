cf9015d9ffe8d5bea91986e89cf412ee60b8259a
Yarin Podoler <yarin.podler@startapp.com>
2014-09-04 12:18:01 -0400

    DEP186 #comment Added context manipulator service

---
+++ test/service/context.manipulator.service.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('Audit Service Unit-Test', function() {
+    var contextManipulatorService;
+
+    require('../../lib/index');
+
+    beforeEach(function () {
+        contextManipulatorService = rewire('../lib/service/audit.service');
+    });
+});
