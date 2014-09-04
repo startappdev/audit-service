caf5eb0873a209671a39f9045c9208bf17b00bb1
Yarin Podoler <yarin.podler@startapp.com>
2014-09-04 15:20:02 -0400

    DEP186 #comment Added test for context manipulation server

--- test/service/context.manipulator.service.spec.js
+++ test/service/context.manipulator.service.spec.js
@@ -2,7 +2,8 @@
 
 describe('Context Manipulator Service Unit-Test', function() {
     var contextManipulatorService,
-        contextService;
+        contextService,
+        sinon = require('sinon');
 
     require('../../lib/index');
 
@@ -20,5 +21,36 @@ describe('Context Manipulator Service Unit-Test', function() {
         contextManipulatorService.addContextManipulator(contextService);
 
         expect(contextManipulatorService.getContextId()).toEqual(1234);
+    });
+
+    it ('should test that data being added to context manipulator', function(done){
+        contextService = {
+            _callback : null,
+            addContextManipulator : function(callback){
+                contextService._callback = callback;
+                return 1234;
+            }
+        };
+
+        var contextManipulator = {
+            getRequest : function(){
+                return {
+                    profile : {
+                        userId : 1234
+                    }
+                };
+            },
+            addData : sinon.stub()
+        };
+
+        contextManipulatorService.addContextManipulator(contextService);
+
+        contextService._callback(contextManipulator, function(){
+            expect(contextManipulator.addData.calledWith({
+                userId : 1234
+            })).toBeTruthy();
+
+            done();
+        });
     });
 });
